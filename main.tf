1f7fd38880fb8be23959b38e555b0d72f8de7c37
Crossur <cruz24955@gmail.com>
2024-07-06 12:15:08 -0400

    cloudbuild test

--- main.tf
+++ main.tf
@@ -12,19 +12,30 @@ terraform {
 locals {
   services = [
     "cloudbuild.googleapis.com",
-    "secretmanager.googleapis.com"
+    "secretmanager.googleapis.com",
+    "container.googleapis.com",
+    "artifactregistry.googleapis.com",
   ]
 }
 
 
-data "google_client_config" "provider" {}
 provider "google" {
   project="terraform-on-gcp-428202"
   credentials = file("credentials.json")
   region="us-east1"
   zone = "us-east1-b"
 }
+
+data "google_client_config" "provider" {}
+
+resource "google_project_service" "enabled_service" {
+  for_each = toset(local.services)
+  project = "terraform-on-gcp-428202"
+  service = each.key
+}
+
 resource "google_container_cluster" "primary" {
+  depends_on = [google_project_service.enabled_service]
   name     = "primary"
   location = "us-east1"
   # We can't create a cluster with no node pool defined, but we want to only use
@@ -34,30 +45,9 @@ resource "google_container_cluster" "primary" {
   initial_node_count       = 1
   deletion_protection = false
 }
-resource "google_project_service" "enabled_service" {
-  for_each = toset(local.services)
-  project = "terraform-on-gcp-428202"
-  service = each.key
-}
-
-
-
-provider "kubectl" {
-  host = "https://${google_container_cluster.primary.endpoint}"
-  token = data.google_client_config.provider.access_token
-  cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
-  load_config_file = false
-}
-
-resource "kubectl_manifest" "deploy" {
-    yaml_body = file("k8s/deploy.yml")
-}
-
-resource "kubectl_manifest" "service" {
-    yaml_body = file("k8s/service.yml")
-}
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
+  depends_on = [google_container_cluster.primary]
   name       = "my-node-pool"
   location   = "us-east1"
   cluster    = google_container_cluster.primary.name
@@ -75,6 +65,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   }
 }
 resource "google_secret_manager_secret" "github-token-secret" {
+  depends_on = [google_project_service.enabled_service]
   secret_id = "github-token-secret"
 
   replication {
@@ -83,6 +74,7 @@ resource "google_secret_manager_secret" "github-token-secret" {
 }
 
 resource "google_secret_manager_secret_version" "github-token-secret-version" {
+  depends_on = [google_secret_manager_secret.github-token-secret]
   secret = google_secret_manager_secret.github-token-secret.id
   secret_data = file("my-github-token.txt")
 }
@@ -96,8 +88,9 @@ data "google_iam_policy" "p4sa-secretAccessor" {
 }
 
 resource "google_secret_manager_secret_iam_policy" "policy" {
-  secret_id = google_secret_manager_secret.github-token-secret.secret_id
-  policy_data = data.google_iam_policy.p4sa-secretAccessor.policy_data
+  project = google_secret_manager_secret.secret-basic.project
+  secret_id = google_secret_manager_secret.secret-basic.secret_id
+  policy_data = data.google_iam_policy.admin.policy_data
 }
 
 resource "google_cloudbuildv2_connection" "my-connection" {
@@ -110,3 +103,19 @@ resource "google_cloudbuildv2_connection" "my-connection" {
     }
   }
 }
+
+provider "kubectl" {
+  host = "https://${google_container_cluster.primary.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
+  load_config_file = false
+}
+
+resource "kubectl_manifest" "deploy" {
+    yaml_body = file("k8s/deploy.yml")
+}
+
+resource "kubectl_manifest" "service" {
+    yaml_body = file("k8s/service.yml")
+}
+
