ab2419e368015075f026100c55f9a42057764996
Crossur <cruz24955@gmail.com>
2024-07-07 18:20:23 -0400

    removed s

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -53,7 +53,7 @@ const Home = (props) => {
                 <div id='wl'></div>
             </div >
             <div id ='valoraid'>VALORAID</div>
-            <button id='play'onClick={newGame}>Play new Gametest3</button>
+            <button id='play'onClick={newGame}>Play new Gametest4</button>
         </div>
     )
 }
